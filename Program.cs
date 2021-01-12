854eb442e193b1a9398569170c88553c74763a26
Josh Mann <jlmann72@gmail.com>
2021-01-12 10:41:58 -0500

    Added greeting method

--- Program.cs
+++ Program.cs
@@ -2,11 +2,30 @@
 
 namespace EmployeeDatabase2
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+
+        static void DisplayGreeting()
+        {
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Welcome to our Employee Database");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+
+            Console.Write("What is your department number? ");
+            var department = int.Parse(Console.ReadLine());
+
+            Console.Write("What is your yearly salary (in dollars)");
+            var salary = int.Parse(Console.ReadLine());
+
+            var salaryPerMonth = salary / 12;
+            Console.WriteLine($"Hello, {name} you make {salaryPerMonth} dollars per month.");
+        }
     }
-  }
 }
