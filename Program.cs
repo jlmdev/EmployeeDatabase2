c539cfc9c33ef99eeed44a30ee9c42434f3f0d91
Josh Mann <jlmann72@gmail.com>
2021-01-12 12:47:10 -0500

    Converted prompts to methods

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace EmployeeDatabase2
 {
     class Program
     {
-
+        // Display Greeting
         static void DisplayGreeting()
         {
             Console.WriteLine("--------------------------------");
@@ -13,21 +13,37 @@ namespace EmployeeDatabase2
             Console.WriteLine();
             Console.WriteLine();
         }
+
+        // Prompt for string
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+
+            return userInput;
+        }
+
+        // Prompt for integer
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = int.Parse(Console.ReadLine());
+
+            return userInput;
+        }
         static void Main(string[] args)
         {
             DisplayGreeting();
 
-            Console.Write("What is your name? ");
-            var name = Console.ReadLine();
+            var name = PromptForString("What is your name? ");
 
             Console.Write("What is your department number? ");
-            var department = int.Parse(Console.ReadLine());
+            int department = PromptForInteger("What is your department number? ");
 
             Console.Write("What is your yearly salary (in dollars)");
-            var salary = int.Parse(Console.ReadLine());
+            int salary = PromptForInteger("What is your yearly salary (in dollars)");
 
-            var salaryPerMonth = salary / 12;
-            Console.WriteLine($"Hello, {name} you make {salaryPerMonth} dollars per month.");
+            Console.WriteLine($"Hello, {name} you make {salary / 12} dollars per month.");
         }
     }
 }
